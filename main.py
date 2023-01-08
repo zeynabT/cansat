9463045bdc2f920775ad289bdf69502bfce4fc7d
saeed srm <rahmani.0109@gmail.com>
2023-01-09 01:59:09 +0330

    clean

--- main.py
+++ main.py
@@ -131,51 +131,6 @@ class Ui(QtWidgets.QMainWindow):
         super(Ui, self).__init__()
         uic.loadUi('CAN-SAT2.ui', self)
 
-        A_angular = "12"
-        A_linear = "20"
-        Pressure = "60"
-        Pressure_1 = "1"
-        Pressure_2 = "2"
-        Pressure_3 = "3"
-        Pressure_4 = "4"
-        Pressure_5 = "5"
-        Pressure_6 = "6"
-
-        InTemp = "2"
-        InTemp_1 = "1"
-        InTemp_2 = "2"
-        InTemp_3 = "3"
-        InTemp_4 = "4"
-        InTemp_5 = "5"
-        InTemp_6 = "6"
-
-        OutTemp = "3"
-        OutTemp_1 = "13"
-        OutTemp_2 = "14"
-        OutTemp_3 = "15"
-        OutTemp_4 = "16"
-        OutTemp_5 = "17"
-        OutTemp_6 = "18"
-
-        Hiumidity = "7"
-        Hiumidity_1 = "8"
-        Hiumidity_2 = "9"
-        Hiumidity_3 = "10"
-        Hiumidity_4 = "11"
-        Hiumidity_5 = "12"
-        Hiumidity_6 = "13"
-
-        AirQuality = "4"
-        UVIndex = "6"
-        # progressValue = secrets.randbelow(100)
-        # print(progressValue)
-
-        CoordinateX = 36.31130898586006
-        CoordinateY = 59.526375931025
-        Coordinate_x = str(CoordinateX)
-        Coordinate_y = str(CoordinateY)
-        coordinate = (CoordinateX, CoordinateY)
-
         Good = "Good"
         Low = "Low"
         Moderate = "Moderate"
@@ -185,68 +140,68 @@ class Ui(QtWidgets.QMainWindow):
 
         # todo make label for every int
         self.LA_angular = self.findChild(QLabel, "label_96")
-        self.LA_angular.setText(A_angular)
+        self.LA_angular.setText(self.A_angular)
         self.LA_linear = self.findChild(QLabel, "label_97")
-        self.LA_linear.setText(A_linear)
+        self.LA_linear.setText(self.A_linear)
         self.LPressure = self.findChild(QLabel, "label_2")
-        self.LPressure.setText(Pressure)
+        self.LPressure.setText(self.Pressure)
         self.LPressure_1 = self.findChild(QLabel, "label_67")
-        self.LPressure_1.setText(Pressure_1)
+        self.LPressure_1.setText(self.Pressure_1)
         self.LPressure_2 = self.findChild(QLabel, "label_69")
-        self.LPressure_2.setText(Pressure_2)
+        self.LPressure_2.setText(self.Pressure_2)
         self.LPressure_3 = self.findChild(QLabel, "label_71")
-        self.LPressure_3.setText(Pressure_3)
+        self.LPressure_3.setText(self.Pressure_3)
         self.LPressure_4 = self.findChild(QLabel, "label_73")
-        self.LPressure_4.setText(Pressure_4)
+        self.LPressure_4.setText(self.Pressure_4)
         self.LPressure_5 = self.findChild(QLabel, "label_75")
-        self.LPressure_5.setText(Pressure_5)
+        self.LPressure_5.setText(self.Pressure_5)
         self.LPressure_6 = self.findChild(QLabel, "label_77")
-        self.LPressure_6.setText(Pressure_6)
+        self.LPressure_6.setText(self.Pressure_6)
 
         self.LInTemp = self.findChild(QLabel, "label")
-        self.LInTemp.setText(InTemp)
+        self.LInTemp.setText(self.InTemp)
         self.LInTemp_1 = self.findChild(QLabel, "label_25")
-        self.LInTemp_1.setText(InTemp_1)
+        self.LInTemp_1.setText(self.InTemp_1)
         self.LInTemp_2 = self.findChild(QLabel, "label_27")
-        self.LInTemp_2.setText(InTemp_2)
+        self.LInTemp_2.setText(self.InTemp_2)
         self.LInTemp_3 = self.findChild(QLabel, "label_29")
-        self.LInTemp_3.setText(InTemp_3)
+        self.LInTemp_3.setText(self.InTemp_3)
         self.LInTemp_4 = self.findChild(QLabel, "label_31")
-        self.LInTemp_4.setText(InTemp_4)
+        self.LInTemp_4.setText(self.InTemp_4)
         self.LInTemp_5 = self.findChild(QLabel, "label_33")
-        self.LInTemp_5.setText(InTemp_5)
+        self.LInTemp_5.setText(self.InTemp_5)
         self.LInTemp_6 = self.findChild(QLabel, "label_35")
-        self.LInTemp_6.setText(InTemp_6)
+        self.LInTemp_6.setText(self.InTemp_6)
 
         self.LOutTemp = self.findChild(QLabel, "label_37")
-        self.LOutTemp.setText(OutTemp)
+        self.LOutTemp.setText(self.OutTemp)
         self.LOutTemp_1 = self.findChild(QLabel, "label_39")
-        self.LOutTemp_1.setText(OutTemp_1)
+        self.LOutTemp_1.setText(self.OutTemp_1)
         self.LOutTemp_2 = self.findChild(QLabel, "label_41")
-        self.LOutTemp_2.setText(OutTemp_2)
+        self.LOutTemp_2.setText(self.OutTemp_2)
         self.LOutTemp_3 = self.findChild(QLabel, "label_43")
-        self.LOutTemp_3.setText(OutTemp_3)
+        self.LOutTemp_3.setText(self.OutTemp_3)
         self.LOutTemp_4 = self.findChild(QLabel, "label_45")
-        self.LOutTemp_4.setText(OutTemp_4)
+        self.LOutTemp_4.setText(self.OutTemp_4)
         self.LOutTemp_5 = self.findChild(QLabel, "label_47")
-        self.LOutTemp_5.setText(OutTemp_5)
+        self.LOutTemp_5.setText(self.OutTemp_5)
         self.LOutTemp_6 = self.findChild(QLabel, "label_49")
-        self.LOutTemp_6.setText(OutTemp_6)
+        self.LOutTemp_6.setText(self.OutTemp_6)
 
         self.LHiumidity = self.findChild(QLabel, "label_50")
-        self.LHiumidity.setText(Hiumidity)
+        self.LHiumidity.setText(self.Hiumidity)
         self.LHiumidity_1 = self.findChild(QLabel, "label_54")
-        self.LHiumidity_1.setText(Hiumidity_1)
+        self.LHiumidity_1.setText(self.Hiumidity_1)
         self.LHiumidity_2 = self.findChild(QLabel, "label_56")
-        self.LHiumidity_2.setText(Hiumidity_2)
+        self.LHiumidity_2.setText(self.Hiumidity_2)
         self.LHiumidity_3 = self.findChild(QLabel, "label_58")
-        self.LHiumidity_3.setText(Hiumidity_3)
+        self.LHiumidity_3.setText(self.Hiumidity_3)
         self.LHiumidity_4 = self.findChild(QLabel, "label_60")
-        self.LHiumidity_4.setText(Hiumidity_4)
+        self.LHiumidity_4.setText(self.Hiumidity_4)
         self.LHiumidity_5 = self.findChild(QLabel, "label_62")
-        self.LHiumidity_5.setText(Hiumidity_5)
+        self.LHiumidity_5.setText(self.Hiumidity_5)
         self.LHiumidity_6 = self.findChild(QLabel, "label_64")
-        self.LHiumidity_6.setText(Hiumidity_6)
+        self.LHiumidity_6.setText(self.Hiumidity_6)
 
         # alarm
         self.alarmText = self.findChild(QLabel, "label_109")
@@ -258,15 +213,15 @@ class Ui(QtWidgets.QMainWindow):
         self.alarm.setStyleSheet("color: rgb(175, 0,3)")
 
         self.LAirQuality = self.findChild(QLabel, "label_52")
-        self.LAirQuality.setText(AirQuality)
+        self.LAirQuality.setText(self.AirQuality)
 
         self.LUVIndex = self.findChild(QLabel, "label_100")
-        self.LUVIndex.setText(UVIndex)
+        self.LUVIndex.setText(self.UVIndex)
 
         self.Coordinate_x = self.findChild(QLabel, "label_103")
-        self.Coordinate_x.setText(Coordinate_x)
+        self.Coordinate_x.setText(str(self.CoordinateX))
         self.Coordinate_y = self.findChild(QLabel, "label_105")
-        self.Coordinate_y.setText(Coordinate_y)
+        self.Coordinate_y.setText(str(self.CoordinateY))
 
         self.airLabel = self.findChild(QLabel, "label_101")
         self.label_101.setFont(QFont('Arial', 10))
@@ -444,7 +399,7 @@ class Ui(QtWidgets.QMainWindow):
         m = folium.Map(
             tiles='Stamen Terrain',
             zoom_start=13,
-            location=coordinate
+            location=self.coordinate
         )
         # save map data to data object
         data = io.BytesIO()
