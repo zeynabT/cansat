dcb9e659a4e7a65e0ddaa86d1d1fd5c4f509edc0
saeed srm <rahmani.0109@gmail.com>
2023-01-21 21:35:45 +0330

    seprate worker

--- main.py
+++ main.py
@@ -18,63 +18,11 @@ import sys
 import threading
 import os
 from PyQt5 import QtCore, QtMultimedia
-import time
+from worker import long_running_function,ProgressWorker
+
+
 CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 
-
-class PercentageWorker(QtCore.QObject):
-    started = QtCore.pyqtSignal()
-    finished = QtCore.pyqtSignal()
-    percentageChanged = QtCore.pyqtSignal(int)
-
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self._percentage = 0
-        value = 0
-
-    @property
-    def percentage(self):
-        return self._percentage
-
-    @percentage.setter
-    def percentage(self, value):
-        if self._percentage == value:
-            return
-        self._percentage = value
-        self.percentageChanged.emit(self.percentage)
-
-    def start(self):
-        self.started.emit()
-
-    def finish(self):
-        self.finished.emit()
-
-
-class FakeWorker:
-    def start(self):
-        pass
-
-    def finish(self):
-        pass
-
-    @property
-    def percentage(self):
-        return 0
-
-    @percentage.setter
-    def percentage(self, value):
-        pass
-
-
-def long_running_function(foo, baz="1", worker=None):
-    if worker is None:
-        worker = FakeWorker()
-    worker.start()
-    while worker.percentage < 100:
-        value = secrets.randbelow(99)  # initializing progress bar
-        worker.percentage = value
-        time.sleep(1)
-        worker.finish()
 
 
 def sound():
@@ -503,7 +451,7 @@ class Ui(QtWidgets.QMainWindow):
         '''
 
     def launch(self):
-        worker = PercentageWorker()
+        worker = ProgressWorker()
         worker.percentageChanged.connect(self.progressBar_4.setValue)
         threading.Thread(
             target=long_running_function,
